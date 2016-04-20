b7230a43e16a97e21cddda406b557c27a8126279
HenryLee <henrylee_cn@foxmail.com>
2016-04-20 16:29:16 +0800

    update

--- lessgo/new.go
+++ lessgo/new.go
@@ -283,15 +283,15 @@ import (
 
 func init() {
 	lessgo.RegMiddleware("打印一些东西", "描述可以省略", func(ctx lessgo.Context) error {
-		ctx.Logger().Info("打印一些东西：1234567890")
+		ctx.Logger().Info("测试中间件-打印一些东西：1234567890")
 		return nil
 	})
 	lessgo.RegMiddleware("显示Header", "描述可以省略", func(ctx lessgo.Context) error {
-		logs.Info("%v", ctx.Request().Header().Keys())
+		logs.Info("测试中间件-显示Header：%v", ctx.Request().Header().Keys())
 		return nil
 	})
 	lessgo.RegMiddleware("故意报错", "描述可以省略", func(ctx lessgo.Context) error {
-		logs.Info("中间件故意报错")
+		logs.Info("测试中间件-故意报错")
 		return errors.New("中间件故意报错 error")
 	})
 }
