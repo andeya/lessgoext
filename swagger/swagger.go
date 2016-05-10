776d9ee5100eb8e1de8d1bbf3e30195ea49cf579
HenryLee <henrylee_cn@foxmail.com>
2016-05-10 10:36:09 +0800

    add tools extend module

--- swagger/swagger.go
+++ swagger/swagger.go
@@ -14,6 +14,7 @@ import (
 	"github.com/lessgo/lessgo"
 	"github.com/lessgo/lessgo/utils"
 	"github.com/lessgo/lessgoext/middleware"
+	"github.com/lessgo/lessgoext/tools/copyfiles"
 )
 
 /*
@@ -32,6 +33,7 @@ var (
 		}
 	}()
 	jsonUrl       = "/swagger.json"
+	dstSwagger    = "./SystemView/Swagger"
 	swaggerHandle = &lessgo.ApiHandler{
 		Desc:    "swagger",
 		Methods: []string{"GET"},
@@ -50,7 +52,7 @@ var (
 			if c.Request().URL().Path() == "/apidoc" {
 				return c.Redirect(302, "/apidoc/index.html")
 			}
-			return c.File(path.Join("Swagger", c.P(0)))
+			return c.File(path.Join(dstSwagger, c.P(0)))
 		},
 	}
 
@@ -76,7 +78,7 @@ func Init() {
 	lessgo.Logger().Sys(`Swagger API doc has been enabled, please access "/apidoc/index.html".` + "\n")
 
 	// 拷贝swagger文件至当前目录下
-	if !utils.FileExists("Swagger") {
+	if !utils.FileExists(dstSwagger) {
 		CopySwaggerFiles()
 	}
 
@@ -269,83 +271,14 @@ type FileInfo struct {
 	Handle  *os.File
 }
 
-// 拷贝swagger文件至当前目录下
+// 拷贝swagger中所有文件至dstSwagger下
 func CopySwaggerFiles() {
-	files_ch := make(chan *FileInfo, 100)
 	fp := filepath.Clean(filepath.Join(os.Getenv("GOPATH"), `\src\github.com\lessgo\lessgoext\swagger\swagger-ui`))
-	go walkFiles(fp, "", files_ch) //在一个独立的 goroutine 中遍历文件
-	os.MkdirAll("Swagger", os.ModeDir)
-	writeFiles("Swagger", files_ch)
+	copyfiles.CopyFiles(fp, dstSwagger, "", copyFunc)
 }
 
-//遍历目录，将文件信息传入通道
-func walkFiles(srcDir, suffix string, c chan<- *FileInfo) {
-	suffix = strings.ToUpper(suffix)
-	filepath.Walk(srcDir, func(f string, fi os.FileInfo, err error) error { //遍历目录
-		if err != nil {
-			lessgo.Logger().Error("%v", err)
-			return err
-		}
-		fileInfo := &FileInfo{}
-		if strings.HasSuffix(strings.ToUpper(fi.Name()), suffix) { //匹配文件
-			if fh, err := os.OpenFile(f, os.O_RDONLY, os.ModePerm); err != nil {
-				lessgo.Logger().Error("%v", err)
-				return err
-			} else {
-				fileInfo.Handle = fh
-				fileInfo.RelPath, _ = filepath.Rel(srcDir, f) //相对路径
-				fileInfo.Size = fi.Size()
-				fileInfo.IsDir = fi.IsDir()
-			}
-			c <- fileInfo
-		}
-		return nil
-	})
-	close(c) //遍历完成，关闭通道
-}
-
-//写目标文件
-func writeFiles(dstDir string, c <-chan *FileInfo) {
-	if err := os.Chdir(dstDir); err != nil { //切换工作路径
-		lessgo.Logger().Fatal("%v", err)
-	}
-	for f := range c {
-		if fi, err := os.Stat(f.RelPath); os.IsNotExist(err) { //目标不存在
-			if f.IsDir {
-				if err := os.MkdirAll(f.RelPath, os.ModeDir); err != nil {
-					lessgo.Logger().Error("%v", err)
-				}
-			} else {
-				if err := ioCopy(f.Handle, f.RelPath); err != nil {
-					lessgo.Logger().Error("%v", err)
-				} else {
-					lessgo.Logger().Info("CP: %v", f.RelPath)
-				}
-			}
-		} else if !f.IsDir { //目标存在，而且源不是一个目录
-			if fi.IsDir() != f.IsDir { //检查文件名被目录名占用冲突
-				lessgo.Logger().Error("%v", "filename conflict:", f.RelPath)
-			} else if fi.Size() != f.Size { //源和目标的大小不一致时才重写
-				if err := ioCopy(f.Handle, f.RelPath); err != nil {
-					lessgo.Logger().Error("%v", err)
-				} else {
-					lessgo.Logger().Info("CP: %v", f.RelPath)
-				}
-			}
-		}
-	}
-	os.Chdir("../")
-}
-
-//复制文件数据
-func ioCopy(srcHandle *os.File, dstPth string) (err error) {
-	defer srcHandle.Close()
-	dstHandle, err := os.OpenFile(dstPth, os.O_CREATE|os.O_WRONLY, os.ModePerm)
-	if err != nil {
-		return err
-	}
-	defer dstHandle.Close()
-
+//复制文件操作函数
+func copyFunc(srcHandle, dstHandle *os.File) (err error) {
 	stat, _ := srcHandle.Stat()
 	if stat.Name() == "index.html" {
 		b, err := ioutil.ReadAll(srcHandle)
