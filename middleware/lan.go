42b1a9df4998f1f62ffe14a2a083408abdb58408
HenryLee <henrylee_cn@foxmail.com>
2016-07-29 11:28:41 +0800

    美化局域网访问限制中间件的响应内容

--- middleware/lan.go
+++ middleware/lan.go
@@ -27,7 +27,16 @@ var OnlyLANAccess = lessgo.ApiMiddleware{
 				bytes.HasPrefix(remoteAddress, lanPrefix_4) {
 				return next(c)
 			}
-			return lessgo.NewHTTPError(http.StatusForbidden, "Only allow LAN access: "+c.RealRemoteAddr())
+
+			s := `<html>
+<head><title>403 Forbidden</title></head>
+<body bgcolor="white">
+<center><h1>403 Forbidden</h1></center>
+<hr><center>Only allow LAN access, your ip is ` + c.RealRemoteAddr() + `.</center>
+</body>
+</html>
+`
+			return c.HTML(http.StatusForbidden, s)
 		}
 	},
 }.Reg()
