ee872d9a29b7cf86088dd876cd618d78047bc531
HenryLee <henrylee_cn@foxmail.com>
2016-08-17 17:45:50 +0800

    update xorm/helper.go

--- dbservice/xorm/helper.go
+++ dbservice/xorm/helper.go
@@ -4,6 +4,10 @@ import (
 	"github.com/go-xorm/xorm"
 )
 
+type Table interface {
+	TableName() string
+}
+
 // 返回设置好表名的数据库session实例
 // 注：不传session参数时，返回的session只能使用一次就会被自动关闭。
 func DBSession(t Table, session ...*xorm.Session) *xorm.Session {
@@ -12,9 +16,11 @@ func DBSession(t Table, session ...*xorm.Session) *xorm.Session {
 	}
 	return DefaultDB().Table(t.TableName())
 }
-
-type Table interface {
-	TableName() string
+func DBSession2(tableName string, session ...*xorm.Session) *xorm.Session {
+	if len(session) > 0 {
+		return session[0].Table(tableName)
+	}
+	return DefaultDB().Table(tableName)
 }
 
 // 无事务的数据库session回调函数
